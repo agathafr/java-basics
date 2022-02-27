e12ad3eeb419f7143505b4c0d22e8025e961f964
Agatha França <agathafranca@outlook.com>
2022-02-27 17:05:12 -0300

    Adds file over syntax, variables, and flow

---
+++ TestVariables.java
@@ -0,0 +1,30 @@
+
+public class TestVariables {
+	
+	// Starting point of application 
+	public static void main(String[] args) {
+		System.out.println("Hello, this is a new test!");
+		
+		// Java is statically and strongly typed
+		// You need to declare all the variables that will be used and
+		// You need to declare the type
+		
+		// declaration
+		int age;
+		
+		//assignment
+		age = 37;
+		
+		System.out.println(age);
+		
+		age = 30 + 10;
+		
+		System.out.println(age);
+		
+		age = 3 * 5 + 2;
+		
+		System.out.println(age);
+		
+		System.out.println("Age is " + age);
+	}
+}
